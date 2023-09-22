79b908a3b71e59a3d2c575210828bb006c889d58
sabarnika <sabarnika1234@gmail.com>
2023-09-22 23:00:30 +0530

    second

--- day3/index.js
+++ day3/index.js
@@ -7,8 +7,70 @@ const fsPromises = require('fs').promises;
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
 const PORT = process.env.PORT || 3500;
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf-8');
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(data);
+  } catch (err) {
+    console.log(err);
+    response.statusCode = 500;
+    response.end();
+  }
+};
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+  const extension = path.extname(req.url);
+  let contentType;
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.jpg':
+      contentType = 'image/jpeg';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.txt':
+      contentType = 'text/plain';
+      break;
+    default:
+      contentType = 'text/html';
+  }
+
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url);
+  if (!extension && req.url.slice(-1) !== '/') filePath += 'html';
+  const fileExist = fs.existsSync(filePath);
+  if (fileExist) {
+    serveFile(filePath, contentType, res);
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { Location: '/new-page.html' });
+        res.end();
+        break;
+      case 'www-page.html':
+        res.writeHead(301, { Location: '/' });
+        res.end();
+        break;
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+    }
+  }
 });
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
